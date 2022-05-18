c009a03f1d004e45708c7202bace74c20ab1d5cf
raulpacheco2k <eu@raulpacheco.com.br>
2022-05-18 20:19:32 -0300

    Melhoria nos testes

--- modules/Financial/Tests/Unit/BankControllerTest.php
+++ modules/Financial/Tests/Unit/BankControllerTest.php
@@ -12,113 +12,124 @@ class BankControllerTest extends TestCase
 
     public function testDigitLessThanAllowed()
     {
-        $bank = Bank::factory()->make();
-        $bank->digit = 0;
+        $bank = Bank::factory()->make([
+            'digit' => 0
+        ]);
 
         $response = $this->json('POST', route('financial.bank.store'), $bank->toArray());
 
 
-        $response->assertJsonValidationErrors('digit');
+        $response->assertJsonValidationErrorFor('digit');
         $response->assertStatus(422);
     }
 
     public function testDigitGreaterThanAllowed()
     {
-        $bank = Bank::factory()->make();
-        $bank->digit = 10;
+        $bank = Bank::factory()->make([
+            'digit' => 10
+        ]);
 
         $response = $this->json('POST', route('financial.bank.store'), $bank->toArray());
 
-        $response->assertJsonValidationErrors('digit');
+        $response->assertJsonValidationErrorFor('digit');
         $response->assertStatus(422);
     }
 
     public function testCodeLessThanAllowed()
     {
-        $bank = Bank::factory()->make();
-        $bank->code = 0;
+        $bank = Bank::factory()->make([
+            'code' => 0
+        ]);
 
         $response = $this->json('POST', route('financial.bank.store'), $bank->toArray());
 
-        $response->assertJsonValidationErrors('code');
+        $response->assertJsonValidationErrorFor('code');
         $response->assertStatus(422);
     }
 
     public function testCodeGreaterThanAllowed()
     {
-        $bank = Bank::factory()->make();
-        $bank->code = 1000;
+        $bank = Bank::factory()->make([
+            'code' => 1000
+        ]);
 
         $response = $this->json('POST', route('financial.bank.store'), $bank->toArray());
 
-        $response->assertJsonValidationErrors('code');
+        $response->assertJsonValidationErrorFor('code');
         $response->assertStatus(422);
     }
 
-    public function testNameNull()
+    public function testNullName()
     {
-        $bank = Bank::factory()->make();
-        $bank->name = null;
+        $bank = Bank::factory()->make([
+            'name' => null
+        ]);
 
         $response = $this->json('POST', route('financial.bank.store'), $bank->toArray());
 
-        $response->assertJsonValidationErrors('name');
+        $response->assertJsonValidationErrorFor('name');
         $response->assertStatus(422);
     }
-
 
     public function testNameLessThanAllowed()
     {
-        $bank = Bank::factory()->make();
-        $bank->name = '';
+        $bank = Bank::factory()->make([
+            'name' => ''
+        ]);
 
         $response = $this->json('POST', route('financial.bank.store'), $bank->toArray());
 
-        $response->assertJsonValidationErrors('name');
+        $response->assertJsonValidationErrorFor('name');
         $response->assertStatus(422);
     }
 
     public function testNameGreaterThanAllowed()
     {
-        $bank = Bank::factory()->make();
-        $bank->name = str_repeat('a', 256);
+        $bank = Bank::factory()->make([
+            'name' => str_repeat('a', 256)
+        ]);
 
         $response = $this->json('POST', route('financial.bank.store'), $bank->toArray());
 
-        $response->assertJsonValidationErrors('name');
+        $response->assertJsonValidationErrorFor('name');
         $response->assertStatus(422);
     }
 
     public function testNameUnique()
     {
-        $bank = Bank::factory()->make();
-        $bank->name = 'Bank Of Testing';
+        $bank = Bank::factory()->make([
+            'name' => 'Bank Of Testing'
+        ]);
         $bank->save();
 
         $response = $this->json('POST', route('financial.bank.store'), $bank->toArray());
 
-        $response->assertJsonValidationErrors('name');
+        $response->assertJsonValidationErrorFor('name');
         $response->assertStatus(422);
     }
 
-    public function testDigitNull()
+    public function testNullDigit()
     {
-        $bank = Bank::factory()->make();
-        $bank->digit = null;
+        $bank = Bank::factory()->make([
+            'digit' => null
+        ]);
 
         $response = $this->json('POST', route('financial.bank.store'), $bank->toArray());
 
         $response->assertStatus(302);
+        $response->assertRedirect(route('financial.bank.index'));
+        $this->assertDatabaseHas('banks', $bank->toArray());
     }
 
-    public function testCodeNull()
+    public function testNullCode()
     {
-        $bank = Bank::factory()->make();
-        $bank->code = null;
+        $bank = Bank::factory()->make([
+            'code' => null
+        ]);
 
         $response = $this->json('POST', route('financial.bank.store'), $bank->toArray());
 
-        $response->assertJsonValidationErrors('code');
+        $response->assertJsonValidationErrorFor('code');
         $response->assertStatus(422);
     }
 }
